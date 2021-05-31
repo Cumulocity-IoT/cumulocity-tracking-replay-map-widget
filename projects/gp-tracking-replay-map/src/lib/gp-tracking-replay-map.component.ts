1a90e594be6dab0666ba655c3796d6bb486caa73
Joshika Agrawal (JOAG) <Joshika.Agrawal@softwareag.com>
2021-05-31 11:52:17 +0530

    checklist doc completed

--- projects/gp-tracking-replay-map/src/lib/gp-tracking-replay-map.component.ts
+++ projects/gp-tracking-replay-map/src/lib/gp-tracking-replay-map.component.ts
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2020 Software AG, Darmstadt, Germany and/or its licensors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as moment_ from 'moment';
 import * as MarkerImage from './marker-icon';
@@ -66,7 +83,7 @@ export class GpTrackingReplayMapComponent implements OnInit, AfterViewInit {
     iconAnchor: [12.5, 41]
   });
 
-  deviceId = '99470470';
+  deviceId;
   initialMaxZoom = 14;
   width: number;
   height: number;
--- projects/gp-tracking-replay-map/src/public-api.ts
+++ projects/gp-tracking-replay-map/src/public-api.ts
@@ -1,7 +1,23 @@
 /*
  * Public API Surface of gp-tracking-replay-map
  */
-
+/**
+ * Copyright (c) 2020 Software AG, Darmstadt, Germany and/or its licensors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 export * from './lib/gp-tracking-replay-map.service';
 export * from './lib/gp-tracking-replay-map.component';
 export * from './lib/gp-tracking-replay-map.module';
